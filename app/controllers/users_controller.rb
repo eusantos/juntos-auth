6c914c4b7e1a1bd84f8ba60eeda22a8367154ed9
Elielson <eusantos11@outlook.com>
2020-02-16 14:49:33 -0300

    testando publicação

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,40 +1,39 @@
 class UsersController < ApplicationController
-  #before_action :set_user, only: [:show, :update, :destroy]
-  #before_action :set_user_token, only: [:show, :update, :destroy]
   before_action :authenticate, except: [:login, :create]
 
   include ActionController::HttpAuthentication::Token::ControllerMethods
-
-#  TOKEN = "meutoken"
 
   def login
     @user = User.find_by(login: params[:login])
     if !@user.nil?
       if @user.password == Digest::MD5.hexdigest(params[:password])
         @user.token = Digest::SHA1.hexdigest([Time.now, rand].join)
-
-      "compara a senha enviada com a senha que tenho gravada"
-      se a saenha ta certa, então eu gero um token e salvo no banco,
-      devolvo um json apenas com o token gerado
+        @user.token_exp = DateTime.now + 3.day
+        @user.save
+        render json: @user, only: [:token, :token_exp]
+      else
+        render json: "{\"Erro\": \"usuario e/ou senha incorretos\"}", status: :unprocessable_entity
+      end
     else 
-      render json: "usuario e/ou senha incorretos", status: :unprocessable_entity
+      render json: "{\"Erro\": \"usuario e/ou senha incorretos\"}", status: :unprocessable_entity
+    end
   end
 
   # GET /users
-  def index
+  def listar
     @users = User.all
-
     render json: @users, except: [:token, :token_exp, :password]
   end
 
   # GET /users/1
-  def show
+  def busca
     render json: @user, except: [:token, :token_exp, :password]
   end
 
   # POST /users
   def create
     @user = User.new(user_params)
+    @user.password = Digest::MD5.hexdigest(@user.password)
     if @user.save
       render json: @user, status: :created, except: [:token, :token_exp, :password], location: @user
     else
@@ -44,24 +43,30 @@ class UsersController < ApplicationController
 
   #Só permite atualizar o password
   # PATCH/PUT /users/1
-  def update
-    if @user.password == Digest::MD5.hexdigest(params[:password]) && @user.login == params[:login] 
-      user.password = params[:new_password] 
-      user.token = nil
+  def mudasenha
+    if(@user.password == Digest::MD5.hexdigest(params[:password]) && @user.login == params[:login]) 
+      @user.password = Digest::MD5.hexdigest(params[:new_password])
+      @user.token = nil
+      if @user.save
+        render json: @user, except: [:token, :token_exp, :password]
+      else
+        render json: @user.errors, status: :unprocessable_entity
+      end
     else
-      render json: "usuario e/ou senha incorretos", status: :unprocessable_entity
-    end
-    
-    if @user.update
-      render json: @user, except: [:token, :token_exp, :password]
-    else
-      render json: @user.errors, status: :unprocessable_entity
+      render json: "{\"Erro\": \"usuario e/ou senha incorretos\"}", status: :unprocessable_entity
+      return
     end
   end
 
   # DELETE /users/1
-  def destroy
-    @user.destroy
+  def remover
+    if !@user.nil?
+      if @user.destroy
+        render json: "{\"Erro\": \"Usuario removido com sucesso\"}"
+      end
+    else
+      render json: "{\"Erro\": \"Sem permissão para remover o usuário\"}"
+    end
   end
 
   private
@@ -70,8 +75,11 @@ class UsersController < ApplicationController
       @user = User.find(params[:id])
     end
 
-    def ser_user_token
-      @user = User.find_by(token: params[:token])
+    def set_user_token
+      authenticate_or_request_with_http_token do |token, option|
+        @user = User.find_by(token: token)
+      end
+      #@user = User.find_by(token: params[:token])
     end
 
     # Only allow a trusted parameter "white list" through.
@@ -84,9 +92,12 @@ class UsersController < ApplicationController
     end
 
     def authenticate
-      #confiro se o existe algum usuario com o token enviado, e se ele ainda é valido, se for, retorna true, se não false
+      #confiro se o existe algum usuario com o token enviado, e se ele ainda é valido, se fo, retorna true, se não false
       set_user_token
-      if @user.token_exp.nil? || @user.token_exp < Datetime.now
+      if @user.nil?
+        return false
+      end
+      if (@user.token_exp.nil? || @user.token_exp < DateTime.now)
         return false
       else
         authenticate_or_request_with_http_token do |token, option|
@@ -94,6 +105,7 @@ class UsersController < ApplicationController
             ::Digest::SHA256.hexdigest(token),
             ::Digest::SHA256.hexdigest(@user.token),
           )
+        end
       end
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,8 @@
 class User < ApplicationRecord
-    require 'digest/md5'
-
     validates_presence_of :login
     validates_presence_of :password
     validates_presence_of :email
     validates :login, uniqueness: { case_sensitive: false }
     validates :email, uniqueness: { case_sensitive: false }
-    before_save :encrypt
 
-    def encrypt
-        self.password = Digest::MD5.hexdigest(password)
-    end
 end
